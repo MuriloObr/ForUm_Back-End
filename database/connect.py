584f20dd27d43a745d751c71b6217f6274a2b940
MuriloObr <muriloobregao@gmail.com>
2023-09-10 13:05:34 -0300

    testing database in planetscale

--- database/connect.py
+++ database/connect.py
@@ -1,3 +1,15 @@
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from os import getenv
 
-engine = create_engine(f"sqlite:///forum.db", echo=True)
+load_dotenv()
+
+server = getenv("DATABASE_HOST")
+username = getenv("DATABASE_USERNAME")
+password = getenv("DATABASE_PASSWORD")
+database = getenv("DATABASE")
+
+print(server, username, password, database)
+
+engine = create_engine(
+    f"mysql+pymysql://{username}:{password}@{server}/{database}", echo=True)
