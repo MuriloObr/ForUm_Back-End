96e025d5d4ef9daaec71137f3e8a3fbbaaee6dc5
MuriloObr <muriloobregao@gmail.com>
2025-05-06 17:19:11 -0300

    sqlmodel engine finished

--- src/db/engine.py
+++ src/db/engine.py
@@ -1,21 +1,23 @@
 from sqlmodel import SQLModel, create_engine
 
-from .models.user import User  
-from .models.post import Post  
-from .models.comment import Comment  
+from .models.user import User  # noqa: F401
+from .models.post import Post  # noqa: F401
+from .models.comment import Comment  # noqa: F401
 
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 from os import getenv
+
+dotenv_path = find_dotenv(".env")
 
 load_dotenv()
 
 isProd = getenv("ISPROD", default=False)
 
-url = getenv("POSTGRES_URL_LOCAL")
+url = getenv("POSTGRES_URL_LOCAL", default="")
 
 if isProd:
-    url = getenv("POSTGRES_URL_PROD")
+    url = getenv("POSTGRES_URL_PROD", default="")
 
-engine = create_engine(url)
+engine = create_engine(url, echo=True)
 
 SQLModel.metadata.create_all(engine)
